387fc0743237e7aa4468ca97d39181945e1683c9
Seymour <ilyamalinaya@mail.ru>
2018-03-06 23:34:22 +0300

    Create Task 2.1 handler

---
+++ CoreDefinitions/Tasks/Cryptography/Crypto_Task2_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using CoreDefinitions.Helpers;
+using CoreDefinitions.Helpers.ConsoleHelper;
+
+
+namespace CoreDefinitions.Tasks.Cryptography
+{
+    class Crypto_Task2_1 : ITask<Crypto_Task2_1>, IBaseTask
+    {
+        TaskAppType _subSystemType;
+        public TaskAppType SubSystemType
+        {
+            get
+            {
+                return _subSystemType;
+            }
+        }
+
+        public Crypto_Task2_1()
+        {
+            _subSystemType = Helpers.TaskAppType.GUI;
+        }
+    }
+}
